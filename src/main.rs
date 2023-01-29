4be082f7f3a67ffbdb9f1df025aa063c4e4bce75
Hannes Donner <hannes@voidbook.fritz.box>
2023-01-29 21:17:39 +0100

    changed comments...

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@
 //! The scene includes a patterned texture and a rotation for visualizing the normals and UVs.
 
 use std::f32::consts::{FRAC_PI_4, PI};
-use bevy_web_asset::WebAssetPlugin;
 use bevy::{
     prelude::*,
     render::render_resource::{Extent3d, TextureDimension, TextureFormat},
@@ -14,7 +13,6 @@ use bevy::sprite::collide_aabb::{collide, Collision};
 use bevy::time::FixedTimestep;
 
 const TIME_STEP: f32 = 1.0 / 60.0;
-
 const PADDLE_SIZE: Vec3 = Vec3::new(2.0, 1.0, 1.0);
 const BRICK_SIZE: Vec3 = Vec3::new(1.0, 0.4, 1.0);
 const GAP_BETWEEN_BRICKS_AND_SIDES: f32 = 0.5;
@@ -23,27 +21,25 @@ const GAP_BETWEEN_BRICKS: f32 = 0.3;
 const GAP_BETWEEN_BRICKS_AND_CEILING: f32 = 0.3;
 const PADDLE_PADDING: f32 = 0.1;
 const GAP_BETWEEN_PADDLE_AND_BRICKS: f32 = 3.0;
-
 const BALL_SIZE: Vec3 = Vec3::new(0.2, 0.2, 0.2);
 const BALL_SPEED: f32 = 7.0;
 const PADDLE_SPEED: f32 = 8.0;
 const WALL_THICKNESS: f32 = 1.0;
 const BALL_STARTING_POSITION: Vec3 = Vec3::new(-4.0, 2.0, 0.0);
 const INITIAL_BALL_DIRECTION: Vec3 = Vec3::new(0.5, -0.5, 0.0);
-// Wall coordinates
 const LEFT_WALL: f32 = 0.0;
 const RIGHT_WALL: f32 = 10.0;
 const TOP_WALL: f32 = 10.0;
 const BOTTOM_WALL: f32 = 0.0;
-
-
 const SCOREBOARD_FONT_SIZE: f32 = 40.0;
 const SCOREBOARD_TEXT_PADDING: Val = Val::Px(5.0);
 
-// Colors
 const WALL_COLOR: Color = Color::rgb(0., 0., 0.);
 const BRICK_COLOR: Color = Color::rgb(0., 0., 0.);
 const SCORE_COLOR: Color = Color::rgb(0.0, 0.0, 0.0);
+
+// Hier sind die Entities und Komponenten die in dem Projekt genutzt werden
+
 #[derive(Component)]
 struct Paddle;
 
@@ -71,15 +67,15 @@ struct WallBundle {
     collider: Collider,
 }
 
-// Which side of the arena is the wall located on?
 enum WallLocation {
     Left,
     Right,
     Bottom,
     Top,
 }
-
+// Ähnlich wie ich in dem Beispiel werden die Wände hier sehr generell implementiert, um das Wiederverwenden dieser einfacher zu machen. 
 impl WallLocation {
+    
     fn position(&self) -> Vec3 {
         match self {
             WallLocation::Left => Vec3::new(LEFT_WALL - (RIGHT_WALL / 2.0), TOP_WALL / 2.0,0.),
@@ -92,7 +88,6 @@ impl WallLocation {
     fn size(&self) -> Vec3 {
         let arena_height = TOP_WALL - BOTTOM_WALL;
         let arena_width = RIGHT_WALL - LEFT_WALL;
-        //assert that constants have legal values
         assert!(arena_height > 0.0);
         assert!(arena_width > 0.0);
         println!("{}. {}", arena_height, arena_width);
@@ -128,6 +123,8 @@ struct Scoreboard {
     score: usize,
 }
 
+
+// Hier werden alle Ressourcen, events und systeme angegeben, welche die App nutzt.
 fn main() {
     App::new()
         .insert_resource(Scoreboard { score: 0})
@@ -148,8 +145,6 @@ fn main() {
         .run();
 }
 
-/// A marker component for our shapes so we can query them separately from the ground plane
-
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
@@ -161,7 +156,8 @@ fn setup(
         base_color_texture: Some(images.add(uv_debug_texture())),
         ..default()
     });
-
+// Das hier ist die Bodenebene
+    
     commands.spawn(PbrBundle {
         mesh: meshes.add(shape::Plane { size: 50. }.into()),
         material: materials.add(Color::SILVER.into()),
@@ -171,7 +167,7 @@ fn setup(
 
     let paddle_y = BOTTOM_WALL + GAP_BETWEEN_PADDLE_AND_FLOOR;
 
-    // light
+    // Lichtquelle
     commands.spawn(PointLightBundle {
         point_light: PointLight {
             intensity: 9000.0,
@@ -182,12 +178,15 @@ fn setup(
         transform: Transform::from_xyz(10.0, 25.0, 10.0),
         ..default()
     });
-    // camera
+    
+    // Kamera-Objekt
     commands.spawn(Camera3dBundle {
         transform: Transform::from_xyz(0.0, 10., 20.0).looking_at(Vec3::new(0., 5., 0.), Vec3::Y),
         ..default()
     });
-    // ball
+    
+    
+    // Der Ball ist auch nur ein PBR, mit den Eigenschaften Velocity und Ball. Dies wird in unteren System (Funktionen) verwendet. 
     commands.spawn((
         PbrBundle {
             mesh: meshes.add(shape::UVSphere::default().into()).into(),
@@ -203,16 +202,24 @@ fn setup(
         Ball,
         Velocity(INITIAL_BALL_DIRECTION.normalize()*BALL_SPEED),
     ));
+    
+    // Wände nutzen ein einfaches Material, welches einfach eine lilane Farbe bekommen.
     let wall_material = materials.add(StandardMaterial{
         base_color: Color::PURPLE,
         ..default()
     });
+
+    // Als Grundobjekt (Mesh) der Wände nutze ich Würfel, welche so skaliert werden, dass sie die Form von länglichen Quadern annehmen
     let wall_mesh: Handle<Mesh> = meshes.add(shape::Cube::default().into()).into();
+
+    // Auf Grund von Rusts Borrow- / Ownershipsystem wird das mesh und Material immer wieder gecloned, da es sonst nicht mehr im Memory wäre.
     commands.spawn(WallBundle::new(WallLocation::Left, wall_material.clone(), wall_mesh.clone()));
     commands.spawn(WallBundle::new(WallLocation::Right, wall_material.clone(), wall_mesh.clone()));
     commands.spawn(WallBundle::new(WallLocation::Bottom, wall_material.clone(), wall_mesh.clone()));
     commands.spawn(WallBundle::new(WallLocation::Top, wall_material.clone(), wall_mesh.clone()));
 
+
+    // Das Paddle ist auch nur ein skalierter Würfel mit den Eigenschaften 'Collider' und 'Paddle', welche von den Systemen zum Querien verwendet werden.
     commands.spawn((
         PbrBundle {
             mesh: meshes.add(shape::Cube::default().into()).into(),
@@ -254,10 +261,11 @@ fn setup(
                 ..default()
             }),
     );
-
+    // Da die Bricks anhand der Konstanten generiert werden, kann es bei falschen Parametern passieren, dass sie eine Größe < 0 haben.
     assert!(BRICK_SIZE.x > 0.0);
     assert!(BRICK_SIZE.y > 0.0);
     assert!(BRICK_SIZE.z > 0.0);
+
     let total_width_of_bricks = (RIGHT_WALL - LEFT_WALL) - 2. * GAP_BETWEEN_BRICKS_AND_SIDES;
     let bottom_edge_of_bricks = paddle_y + GAP_BETWEEN_PADDLE_AND_BRICKS;
     let total_height_of_bricks = TOP_WALL - bottom_edge_of_bricks - GAP_BETWEEN_BRICKS_AND_CEILING;
@@ -265,23 +273,21 @@ fn setup(
     assert!(total_width_of_bricks > 0.0);
     assert!(total_height_of_bricks > 0.0);
 
-    // Given the space available, compute how many rows and columns of bricks we can fit
+    // Ich berechne wie viele Reihen und Spalten an Bricks es geben kann
     let n_columns = (total_width_of_bricks / (BRICK_SIZE.x + GAP_BETWEEN_BRICKS)).floor() as usize;
     let n_rows = (total_height_of_bricks / (BRICK_SIZE.y + GAP_BETWEEN_BRICKS)).floor() as usize;
     let n_vertical_gaps = n_columns - 1;
 
-    // Because we need to round the number of columns,
-    // the space on the top and sides of the bricks only captures a lower bound, not an exact value
+    // Da es Spalten und Reihen nur als ganze Zahl geben, wird das hier sichergestellt.
     let center_of_bricks = 0.0;
     let left_edge_of_bricks = center_of_bricks
-        // Space taken up by the bricks
         - (n_columns as f32 / 2.0 * BRICK_SIZE.x)
-        // Space taken up by the gaps
         - n_vertical_gaps as f32 / 2.0 * GAP_BETWEEN_BRICKS;
 
     let offset_x = left_edge_of_bricks + BRICK_SIZE.x / 2.;
     let offset_y = bottom_edge_of_bricks + BRICK_SIZE.y / 2.;
 
+    // Hier werden die Bricks anhand der vorher berechneten Werte generiert.
     for row in 0..n_rows {
         for column in 0..n_columns {
             let brick_position = Vec2::new(
@@ -308,6 +314,7 @@ fn setup(
     }
 }
 
+// Alle Entities mit der Komponente 'Paddle' sollen sich um ihre eigene Y-Achse drehen.
 fn rotate(mut query: Query<&mut Transform, With<Paddle>>, time: Res<Time>) {
     for mut transform in &mut query {
         transform.rotate_y(time.delta_seconds() / 2.);
@@ -319,6 +326,7 @@ fn update_scoreboard(scoreboard: Res<Scoreboard>, mut query: Query<&mut Text>) {
     text.sections[1].value = scoreboard.score.to_string();
 }
 
+// Alle Objekte mit der Komponente 'Paddle' können mit dem Keyboard bewegt werden.
 fn move_object(mut query: Query<&mut Transform, With<Paddle>>, keyboard_input: Res<Input<KeyCode>>){
     let mut direction = 0.0;
     let mut object_transform = query.single_mut();
@@ -336,7 +344,8 @@ fn move_object(mut query: Query<&mut Transform, With<Paddle>>, keyboard_input: R
 
     object_transform.translation.x = new_object_positiion.clamp(left_bound, right_bound);
 }
-/// Creates a colorful test pattern
+
+// Diese Textur habe ich als Beispiel aus einem anderen Beispiel gefunden, welche das Verhalten von 3D Objekten gezeigt habe, ich fande es passend und habe es übernommen.
 fn uv_debug_texture() -> Image {
     const TEXTURE_SIZE: usize = 8;
 
@@ -363,7 +372,7 @@ fn uv_debug_texture() -> Image {
         TextureFormat::Rgba8UnormSrgb,
     )
 }
-
+// Wir verschieben das Objekte einfach anhand der Velocity und Framerate.
 fn apply_velocity(mut query: Query<(&mut Transform, &Velocity)>) {
     for (mut transform, velocity) in &mut query {
         transform.translation.x += velocity.x * TIME_STEP;
@@ -379,8 +388,9 @@ fn check_for_collision(
     collider_query: Query<(Entity, &Transform, Option<&Brick>), With<Collider>>,
     mut collision_events: EventWriter<CollisionEvent>,
 ) {
-
+    // Da es nur einen Ball gibt, können wir der Engine sagen, dass wir nur das erste Objekt aus dem 'ball_query' haben wollen
     let (mut ball_velocity, ball_transform)  = ball_query.single_mut();
+
     for (collider_entity, transform, maybe_brick) in &collider_query {
         let collision = collide(
             ball_transform.translation,
@@ -388,27 +398,33 @@ fn check_for_collision(
             transform.translation,
             transform.scale.truncate(),
         );
-
+        // Some() lässt sich wie 'Any' in Python lesen,  ~ collision != null
         if let Some(collision) = collision {
 
             collision_events.send_default();
+
+            // Falls das Objekt mit dem kollidiert wird ein Brick ist, soll das Scoreboard geupdated werden und der Brick entfernt werden
             if maybe_brick.is_some() {
 
                 scoreboard.score += 1;
                 commands.entity(collider_entity).despawn();
             }
 
+            // standardmäßig soll die Richtung nicht verändert werden.
             let mut reflect_x = false;
             let mut reflect_y = false;
 
+
+            // match ist eine Struktur in Rust, bei welcher man eine Funktion anhand des current value einer enum ausführen kann
+            // Wir stellen sicher, dass der Ball von innerhalb des Spiels kommt/
             match collision {
                 Collision::Left => reflect_x = ball_velocity.x > 0.0,
                 Collision::Right => reflect_x = ball_velocity.x < 0.0,
                 Collision::Top => reflect_y = ball_velocity.y < 0.0,
                 Collision::Bottom => reflect_y = ball_velocity.y > 0.0,
-                Collision::Inside => { /* do nothing */ }
+                Collision::Inside => {  }
             }
-
+            // Falls true, wird die x-Velocity gespiegelt
             if reflect_x {
                 ball_velocity.x = -ball_velocity.x;
             }
